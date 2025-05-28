c4a8d3b34208d3d03b12da9abee1d716bd5d1b90
dlgkiet <giakiet20012004@gmail.com>
2025-05-28 19:10:55 +0700

    Sửa lỗi TypeScript trong theme-provider và các cảnh báo ESLint

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,7 +1,14 @@
-"use client";
-
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import type { ThemeProviderProps } from "next-themes";
+import type { Attribute } from "next-themes"; // Nhập type Attribute từ next-themes
+import { PropsWithChildren } from "react";
+
+// Định nghĩa ThemeProviderProps với Attribute từ next-themes
+type ThemeProviderProps = PropsWithChildren<{
+  attribute?: Attribute | Attribute[] | undefined;
+  defaultTheme?: string | undefined;
+  enableSystem?: boolean | undefined;
+  disableTransitionOnChange?: boolean | undefined;
+}>;
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
