ce7dd01c68451190fa6d9e1f37dc905061f54ece
dlgkiet <giakiet20012004@gmail.com>
2025-05-31 10:42:35 +0700

    fix gg analytic

---
+++ pages/_app.tsx
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import { AppProps } from "next/app";
+import { useEffect } from "react";
+import Script from "next/script";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement!.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-912QM9EFWV"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-912QM9EFWV');
+          `}
+        </Script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
